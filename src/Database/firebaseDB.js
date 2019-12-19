8a1b2b6f49ac28c1b2cb6386ca50c7f84c5f03c4
mcadamia20 <58778392+mcadamia20@users.noreply.github.com>
2019-12-19 17:04:46 +0000

    Update firebaseDB.js

--- src/Database/firebaseDB.js
+++ src/Database/firebaseDB.js
@@ -1,4 +1,7 @@
-const firebaseconfig = [
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const firebaseconfig = 
     {
     apiKey: "AIzaSyDp61_EFLlrABIxTtdAYODk5jqWYkCt9Qw",
     authDomain: "pluto-beauty.firebaseapp.com",
@@ -8,5 +11,19 @@ const firebaseconfig = [
     messagingSenderId: "718258830394",
     appId: "1:718258830394:web:9e0084cc5e2aa557b4c903",
     measurementId: "G-XCT0LY3WS8"
-    }
-]
+    };
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseconfig);
+    this.auth = app.auth();
+  }
+    // *** Auth API ***
+  createUserWithMailPass = (email, password) =>
+    this.auth.createUserWithMailPass(email, password);
+  signInWithMailPass = (email, password) =>
+    this.auth.signInWithMailPass(email, password);
+   signOut = () => this.auth.signOut();
+  passwordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+}
+export default Firebase;
