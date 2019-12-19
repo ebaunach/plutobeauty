32b3e09f5b7528619a056f8bf0b91d5851fc3d8f
mcadamia20 <58778392+mcadamia20@users.noreply.github.com>
2019-12-19 17:37:21 +0000

    Update SignIn.js

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Router, Route } from "react-router-dom";
 import { SignUpLink } from '../SignUp';
 import { withFirebase } from '../Firebase';
 
