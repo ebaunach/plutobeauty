f554907aaa9d6206d37f72a9fc2d165f740a046e
mcadamia20 <58778392+mcadamia20@users.noreply.github.com>
2019-12-19 16:50:27 +0000

    Update SignIn.js

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,6 +1,20 @@
-import React, {Component} from 'react';
-class SignIn extends Component{
+import React, { Component } from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseconfig from './firebaseDB';
+import './SignInUp.css';
+
+const firebaseApp = firebase.initializeApp(firebaseconfig);
+
+class SignInUp extends Component{
     render() {
+    const {
+      user,
+      signOut,
+      signInWithMailPass,
+    } = this.props;
+
         return(
             <div className="welcome">
                 <h1>Welcome to Pluto Beauty </h1>
@@ -8,11 +22,19 @@ class SignIn extends Component{
                 <h3>Sign in or sign up below to start reviewing!</h3>
 
                 /**TO DO - use firebase **/
-                <div className="logIn">
-                    <p>TO DO</p>
-                </div>
+                <div className="signIn">
+                  {
+                    user 
+                    ? <p>Hello, {user.displayName}</p>
+                    : <p>Please sign in.</p>
+                  }
+                  {
+                    user
+                    ? <button onClick={signOut}>Sign out</button>
+                    : <button onClick={signInWithMailPass}>Sign in with Your Email</button>
+                  </div>
 
-                /**TO DO - use firebase **/
+                /**TO DO - link to sign up page if they dont have account **/
                 <div className="signUp">
                     <p>TO DO</p>
                 </div>
